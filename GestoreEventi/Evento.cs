5d04bab63f00509e6d72198ac396c0eca164692f
Davide Petrillo <davidepetrilloo@outlook.it>
2022-04-25 19:51:07 +0200

    Aggiungere i file di progetto.

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        internal string titoloEvento;
+        internal DateTime dataDellEvento;
+        internal int capienzaMassimaEvento;
+        internal int numeroPostiPrenotati;
+
+        // costruttore
+
+        public Evento(string titoloEvento, DateTime dataDellEvento, int capienzaMassimaEvento)
+        {
+            if (dataDellEvento < DateTime.Now)
+            {
+                throw new InvalidDataException("La data dell'evento è precedente alla data di oggi.");
+            }
+            this.dataDellEvento = dataDellEvento;
+
+            if (titoloEvento == null)
+            {
+                throw new ArgumentNullException("Mi dispiace ma non hai inserito un titolo all'evento");
+            }
+            this.titoloEvento = titoloEvento;
+
+            if(capienzaMassimaEvento < 0)
+            {
+                throw new Exception("Mi dispiace ma la capienza dei posti per l'evento non può essere negativa");
+            }
+            this.capienzaMassimaEvento = capienzaMassimaEvento;
+
+            this.numeroPostiPrenotati = 0;
+        }
+
+        // Getter e setter titoloEvento
+        public string GetTitoloEvento(string nuovoTitoloEvento)
+        {
+            return nuovoTitoloEvento;
+        }
+        public string SetTitoloEvento(string nuovoTitoloEvento)
+        {
+            return nuovoTitoloEvento;
+        }
+
+        // Getter e setter dataDellEvento
+        public string GetDataDellEvento(string nuovaDataDellEvento)
+        {
+            return nuovaDataDellEvento;
+        }
+        public string SetDataDellEvento(string nuovaDataDellEvento)
+        {
+            return nuovaDataDellEvento;
+        }
+
+        // Getter capienzaMassimaEvento
+        public int GetCapienzaMassimaEvento(int nuovaCapienzaMassimaEvento)
+        {
+            return nuovaCapienzaMassimaEvento;
+        }
+
+        // Getter numeroPostiPrenotati
+        public int GetNumeroPostiPrenotati (int nuovoNumeroPostiPrenotati)
+        {
+            return nuovoNumeroPostiPrenotati;
+        }
+
+        public void PrenotaPosti(int numeroPostiPrenotatiDallUtente)
+        {
+            numeroPostiPrenotati = numeroPostiPrenotati + numeroPostiPrenotatiDallUtente;
+            Console.WriteLine($"Numero di posti prenotati: {numeroPostiPrenotati}");
+            Console.WriteLine($"Numero di posti disponibili: {capienzaMassimaEvento - numeroPostiPrenotati}");
+        }
+
+        public void DisdiciPosti(int numeroPostiDisdettiDallUtente)
+        {
+            numeroPostiPrenotati = numeroPostiPrenotati - numeroPostiDisdettiDallUtente;
+            Console.WriteLine($"Numero di posti prenotati: {numeroPostiPrenotati}");
+            Console.WriteLine($"Numero di posti disponibili: {capienzaMassimaEvento - numeroPostiPrenotati}");
+        }
+
+
+        public virtual void ToString()
+        {
+            Console.WriteLine();
+            Console.WriteLine("--- Evento ---");
+            Console.WriteLine();
+            Console.WriteLine($"{dataDellEvento} - {titoloEvento}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ GestoreEventi/Program.cs
@@ -0,0 +1,56 @@
+﻿using GestoreEventi;
+
+Console.Write("Inserisci il nome dell'evento: ");
+string titoloEventoSceltoDallUtente = Console.ReadLine();
+
+Console.Write("Inserisci la data dell'evento: ");
+DateTime dataDellEventoSceltaDallUtente = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Inserisci la capienza massima dell'evento: ");
+int capienzaMassimaEventoSceltaDallUtente = int.Parse(Console.ReadLine());
+
+Console.Write("Quanti posti desideri prenotare? ");
+int numeroPostiPrenotatiDallUtente = int.Parse(Console.ReadLine());
+
+Evento nuovoEvento = new Evento(titoloEventoSceltoDallUtente, dataDellEventoSceltaDallUtente, capienzaMassimaEventoSceltaDallUtente);
+
+nuovoEvento.PrenotaPosti(numeroPostiPrenotatiDallUtente);
+
+    Console.Write("Vuoi disdire dei posti [si/no] ? ");
+    string sceltaDisdettaUtente = Console.ReadLine();
+
+    if (sceltaDisdettaUtente == "si")
+    {
+        Console.Write("Inserisci il numero di posti da disdire: ");
+        int sceltaNumeroPostiDisdettiDallUtente = int.Parse(Console.ReadLine());
+        nuovoEvento.DisdiciPosti(sceltaNumeroPostiDisdettiDallUtente);
+    } else if (sceltaDisdettaUtente == "no")
+{
+    nuovoEvento.ToString();
+}
+
+nuovoEvento.ToString();
+
+
+
+
+
+
+
+
+
+
+/*
+Console.WriteLine();
+Console.WriteLine($"Numero di posti prenotati: {numeroPostiPrenotatiDallUtente}");
+Console.WriteLine($"Numero di posti disponibili: {capienzaMassimaEventoSceltaDallUtente - numeroPostiPrenotatiDallUtente}");
+Console.WriteLine();
+
+
+nuovoEvento.DisdiciPosti(numeroPostiPrenotatiDallUtente);
+
+Console.WriteLine();
+Console.WriteLine($"Numero di posti prenotati: {numeroPostiPrenotatiDallUtente}");
+Console.WriteLine($"Numero di posti disponibili: {capienzaMassimaEventoSceltaDallUtente - numeroPostiPrenotatiDallUtente}");
+Console.WriteLine();
+*/
